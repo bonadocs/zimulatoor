3e301fbf034df9f58aacf0bfa97a8e7d433bae7e
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2024-03-26 07:05:38 +0800

    Implement core simulation engine
    Signed-off-by: Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>

---
+++ src/simulation/index.ts
@@ -0,0 +1,138 @@
+﻿import { EvmError, EVMResult } from '@ethereumjs/evm'
+import {
+  AccessListEIP2930Transaction,
+  FeeMarketEIP1559Transaction,
+  LegacyTransaction,
+  TypedTransaction,
+} from '@ethereumjs/tx'
+import { Address, bytesToHex, hexToBytes } from '@ethereumjs/util'
+import { RunTxResult, VM } from '@ethereumjs/vm'
+import { JsonRpcProvider, TransactionRequest } from 'ethers'
+
+import { initializeSimulationTransaction } from '../tx'
+import { addErrorMessage } from '../util'
+import { createVM } from '../vm'
+
+import { populateTransaction } from './tx'
+
+type TxResult = RunTxResult & {
+  error?: EvmError & { message?: string }
+  logs?: Array<{ address: string; topics: string[]; data: string }>
+}
+
+type CallResult = EVMResult & {
+  error?: EvmError & { message?: string }
+}
+
+export class SimulationEngine {
+  readonly #jsonRpcProvider: JsonRpcProvider
+  readonly #vm: VM
+
+  private constructor(jsonRpcProvider: JsonRpcProvider, vm: VM) {
+    this.#jsonRpcProvider = jsonRpcProvider
+    this.#vm = vm
+  }
+
+  static async create(jsonRpcProvider: JsonRpcProvider, blockNumber?: bigint) {
+    blockNumber ??= BigInt(await jsonRpcProvider.getBlockNumber())
+    const vm = await createVM({
+      providerUrl: jsonRpcProvider._getConnection().url,
+      blockNumber,
+    })
+    return new SimulationEngine(jsonRpcProvider, vm)
+  }
+
+  async setBalance(address: string, balance: bigint) {
+    const addr = Address.fromString(address)
+    const account = await this.#vm.stateManager.getAccount(addr)
+    if (!account) {
+      await this.#vm.stateManager.putAccount(addr)
+    }
+
+    await this.#vm.stateManager.modifyAccountFields(addr, {
+      balance,
+    })
+  }
+
+  async setStorage(address: string, key: string, value: string) {
+    const addr = Address.fromString(address)
+    await this.#vm.stateManager.putContractStorage(
+      addr,
+      hexToBytes(key),
+      hexToBytes(value),
+    )
+  }
+
+  async execute(tx: TransactionRequest): Promise<TxResult> {
+    const populated = await populateTransaction({
+      vm: this.#vm,
+      provider: this.#jsonRpcProvider,
+      tx,
+    })
+
+    let typedTx: TypedTransaction
+    switch (populated.type) {
+      case '0x2':
+        typedTx = initializeSimulationTransaction(FeeMarketEIP1559Transaction, {
+          ...populated,
+          from: tx.from as string,
+          gasPrice: undefined,
+          maxFeePerGas: populated.maxFeePerGas!,
+          gasLimit: populated.gas,
+        })
+        break
+      case '0x1':
+        typedTx = initializeSimulationTransaction(
+          AccessListEIP2930Transaction,
+          {
+            ...populated,
+            from: tx.from as string,
+            gasLimit: populated.gas,
+          },
+        )
+        break
+      default:
+        typedTx = initializeSimulationTransaction(LegacyTransaction, {
+          ...populated,
+          from: tx.from as string,
+          gasLimit: populated.gas,
+        })
+    }
+
+    const runTxResult = await this.#vm.runTx({ tx: typedTx, skipBalance: true })
+    const parsedLogs = runTxResult.execResult.logs?.map(
+      ([address, topics, data]) => ({
+        address: bytesToHex(address),
+        topics: topics.map(bytesToHex),
+        data: bytesToHex(data),
+      }),
+    )
+
+    addErrorMessage(runTxResult.execResult)
+    return {
+      ...runTxResult,
+      error: runTxResult.execResult.exceptionError,
+      logs: parsedLogs,
+    }
+  }
+
+  async call(tx: {
+    from: string
+    to: string
+    data: string
+  }): Promise<CallResult> {
+    if (!tx.to) {
+      throw new Error("'to' address is required")
+    }
+
+    const evmResult = await this.#vm.evm.runCall({
+      caller: Address.fromString(tx.from),
+      to: Address.fromString(tx.to),
+      data: hexToBytes(tx.data),
+      isStatic: true,
+    })
+
+    addErrorMessage(evmResult.execResult)
+    return { ...evmResult }
+  }
+}
---
+++ src/simulation/tx.ts
@@ -0,0 +1,86 @@
+﻿import { FeeMarketEIP1559Transaction } from '@ethereumjs/tx'
+import { Address } from '@ethereumjs/util'
+import { VM } from '@ethereumjs/vm'
+import {
+  copyRequest,
+  isAddress,
+  JsonRpcProvider,
+  TransactionRequest,
+  Wallet,
+} from 'ethers'
+
+import { initializeSimulationTransaction } from '../tx'
+
+import { SimulationError } from './error'
+
+export type PopulateTransactionOptions = {
+  vm: VM
+  provider: JsonRpcProvider
+  tx: TransactionRequest
+}
+
+async function getAccountNonce(vm: VM, address: string) {
+  const account = await vm.stateManager.getAccount(Address.fromString(address))
+  return Number(account?.nonce ?? BigInt(0))
+}
+
+async function estimateGas(vm: VM, tx: TransactionRequest) {
+  try {
+    await vm.stateManager.checkpoint()
+    const result = await vm.runTx({
+      tx: initializeSimulationTransaction(FeeMarketEIP1559Transaction, {
+        from: tx.from as string,
+        to: tx.to as string,
+        data: tx.data as string,
+        gasLimit: 10_000_000,
+        maxFeePerGas: 10,
+      }),
+      skipBalance: true,
+      skipNonce: true,
+      skipBlockGasLimitValidation: true,
+      skipHardForkValidation: true,
+    })
+    return `0x${result.totalGasSpent.toString(16)}`
+  } finally {
+    await vm.stateManager.revert()
+  }
+}
+
+export async function populateTransaction({
+  vm,
+  provider,
+  tx,
+}: PopulateTransactionOptions) {
+  tx = copyRequest(tx)
+  if (!isAddress(tx.from)) {
+    throw new SimulationError('Invalid from address')
+  }
+
+  const from = tx.from
+  tx.nonce = await getAccountNonce(vm, from)
+
+  // cache gas limit to bypass the check
+  const gasLimit = tx.gasLimit
+  tx.gasLimit = 1
+
+  // Hack from address to populate the transaction
+  const dummyWalletForPopulate = new Wallet(
+    '0x0101010101010101010101010101010101010101010101010101010101010101',
+    provider,
+  )
+  tx.from = dummyWalletForPopulate.address
+  const rpcTx = provider.getRpcTransaction(
+    await dummyWalletForPopulate.populateTransaction(tx),
+  )
+  rpcTx.from = tx.from = from
+
+  rpcTx.gas = gasLimit?.toString()
+
+  // estimate gas if not provided. skip if it's a contract creation
+  if (rpcTx.gas == null && rpcTx.to) {
+    rpcTx.gas = await estimateGas(vm, tx)
+  } else if (!rpcTx.to) {
+    rpcTx.gas = '0x' + (10_000_000).toString(16)
+  }
+  return rpcTx
+}
